a593b6b0500b6d79f94944c47ebeef81f6e053f0
iguillenp <ibaiguillenpacho@gmail.com>
2026-03-10 20:58:25 +0100

    fix: var name

--- mustdrifter/preprocessing/lang_detection.py
+++ mustdrifter/preprocessing/lang_detection.py
@@ -8,25 +8,25 @@ logger = logging.getLogger(__name__)
 MODEL_URL = "https://dl.fbaipublicfiles.com/fasttext/supervised-models/lid.176.ftz"
 MODEL_PATH = Path(__file__).resolve().parent / "lid.176.ftz"
 
-_lid = None
+lid = None
 
 
 def _get_lid():
-    global _lid
+    global lid
     logger.info("Loading language detection model...")
 
     if not MODEL_PATH.exists():
         urllib.request.urlretrieve(MODEL_URL, str(MODEL_PATH))
-    _lid = fasttext.load_model(str(MODEL_PATH))
+    lid = fasttext.load_model(str(MODEL_PATH))
     logger.info("Language detection model loaded successfully.")
-    return _lid
+    return lid
 
 
 def detect_lang(text):
     logger.debug(f"Detecting language for text: {text[:30]}...")  # Log the beginning of the text for context
     if not isinstance(text, str) or len(text.strip()) < 5:
         return "und"
-    if _lid is None: lid = _get_lid()
+    if lid is None: lid = _get_lid()
     label, prob = lid.predict(text.replace("\n", " "), k=1)
     logger.debug(f"Predicted label: {label}, probability: {prob}")
     return label[0].replace("__label__", "")
