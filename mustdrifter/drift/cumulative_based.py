a36660482561d6a2ac1e4c82dfedde4fa09fe9c0
iguillenp <ibaiguillenpacho@gmail.com>
2026-03-12 19:26:36 +0100

    different p_values

--- mustdrifter/drift/cumulative_based.py
+++ mustdrifter/drift/cumulative_based.py
@@ -34,7 +34,9 @@ def ks_drift(reference_sample, test_sample, filename):
     logger.info("KS drift detection completed.")
 
     with open(filename, "w") as f:
-        json.dump({"magnitude": np.median(statistics), "p_value": np.median(p_values)}, f)
+        json.dump({"magnitude": np.median(statistics), "p_value": np.median(p_values), 
+                   "p_value_median": np.median(p_values), "p_value_mean": np.mean(p_values),
+                   "p_value_min": np.min(p_values), "p_value_max": np.max(p_values)}, f)
 
     logger.info(f"KS drift results saved to {filename}")
 
